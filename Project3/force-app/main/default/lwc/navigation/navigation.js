56e1f2d92b4e9b4c3938ed4a727bc59f75ae112e
Anthony Schultz <georgeschultz1998@gmail.com>
2024-05-21 17:48:25 -0400

    Created Navigation LWC Component

--- Project3/force-app/main/default/lwc/navigation/navigation.js
+++ Project3/force-app/main/default/lwc/navigation/navigation.js
@@ -1,3 +1,57 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import isGuest from '@salesforce/user/isGuest';
+import logo from '@salesforce/resourceUrl/logo'; 
 
-export default class Navigation extends LightningElement {}
+export default class Navigation extends NavigationMixin(LightningElement) {
+  isGuest = false; // Update this based on your logic
+  isLoggedIn = !isGuest;
+  get logoUrl() {
+    return logo;
+  }
+  navigateToHome(event) {
+    event.preventDefault();
+    this[NavigationMixin.Navigate]({
+      type: 'standard__namedPage',
+      attributes: {
+        pageName: 'home'
+      }
+    });
+  }
+
+  navigateToTourSignUp(event) {
+    event.preventDefault();
+    this[NavigationMixin.Navigate]({
+      type: 'standard__namedPage',
+      attributes: {
+        pageName: 'tour'
+      }
+    });
+  }
+
+  navigateToSignUp(event) {
+    event.preventDefault();
+    this[NavigationMixin.Navigate]({
+      type: 'standard__namedPage',
+      attributes: {
+        pageName: 'signup'
+      }
+    });
+  }
+
+  navigateToLogin(event) {
+    event.preventDefault();
+    this[NavigationMixin.Navigate]({
+      type: 'standard__namedPage',
+      attributes: {
+        pageName: 'login'
+      }
+    });
+  }
+
+  handleLogout(event) {
+    event.preventDefault();
+    // Implement logout logic here, such as redirecting to a logout URL
+    window.location.href = '/secur/logout.jsp';
+  }
+}
